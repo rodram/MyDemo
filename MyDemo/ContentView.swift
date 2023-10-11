0601d1d2f5bad1669493a48b40a19d9dbc7e60e5
Rodrigo Ramos <rodrigo@figurasthlm.se>
2023-10-11 08:57:56 +0200

    Initial Commit

---
+++ MyDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyDemo
+//
+//  Created by Rodrigo Ramos on 2023-10-11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyDemo/MyDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyDemoApp.swift
+//  MyDemo
+//
+//  Created by Rodrigo Ramos on 2023-10-11.
+//
+
+import SwiftUI
+
+@main
+struct MyDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
